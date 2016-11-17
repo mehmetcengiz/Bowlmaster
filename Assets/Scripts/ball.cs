ba37af161f1c9b3b3b02c82ab4a633ffc39d1cca
Mehmet Cengiz <m.cengiz4716@gmail.com>
2016-11-17 22:37:49 +0300

    Ball is launching

---
+++ Assets/Scripts/ball.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ball : MonoBehaviour {
+
+    public float launchSpeed = 600f;
+
+    private Rigidbody rigidbody;
+    private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+	    rigidbody = GetComponent<Rigidbody>();
+	    audioSource = GetComponent<AudioSource>();
+
+        Launch();
+	}
+
+    private void Launch() {
+        rigidbody.velocity = new Vector3(0, 0, launchSpeed);
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+	void Update () {
+	}
+}
