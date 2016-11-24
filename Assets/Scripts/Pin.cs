4d84454075962c1e8fa0045805942dc3f53fbb9c
Mehmet Cengiz <m.cengiz4716@gmail.com>
2016-11-25 02:03:44 +0300

    Counting Upright Objects

--- Assets/Scripts/Pin.cs
+++ Assets/Scripts/Pin.cs
@@ -11,7 +11,6 @@ public class Pin : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	    print(name + " " +IsStanding());
 	}
 
     public bool IsStanding() {
---
+++ Assets/Scripts/PinSetter.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PinSetter : MonoBehaviour {
+
+    public Text standingDisplay;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    standingDisplay.text = CountStanding().ToString();
+	}
+
+    public int CountStanding() {
+        int countOfStandingPins =0;
+        foreach (Pin pin in GameObject.FindObjectsOfType<Pin>()) {
+            if (pin.IsStanding()) {
+                countOfStandingPins++;
+            }
+        }
+        return countOfStandingPins;
+    }
+}
