44cc4e35b025cf9f6103a96fa2a2c4df8c877523
Mehmet Cengiz <m.cengiz4716@gmail.com>
2016-11-27 03:40:15 +0300

    Control system finished.

--- Assets/Editor/ActionMasterTest.cs
+++ Assets/Editor/ActionMasterTest.cs
@@ -7,6 +7,8 @@ public class ActionMasterTest {
 
     private ActionMaster.Action endTurn = ActionMaster.Action.EndTurn;
     private ActionMaster.Action tidy = ActionMaster.Action.Tidy;
+    private ActionMaster.Action reset = ActionMaster.Action.Reset;
+    private ActionMaster.Action endGame = ActionMaster.Action.EndGame;
 
     private ActionMaster actionMaster;
 
@@ -34,5 +36,43 @@ public class ActionMasterTest {
     public void T03Bowl2n8ReturnsEndTurn() {
         actionMaster.Bowl(8);
         Assert.AreEqual(endTurn,actionMaster.Bowl(2));
+    }
+
+    [Test]
+    public void T04CheckResetAtStrikeInLastFrame() {
+        int[] rolls = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+        foreach (int roll in rolls) {
+            actionMaster.Bowl(roll);
+        }
+        Assert.AreEqual(reset,actionMaster.Bowl(10));
+    }
+    [Test]
+    public void T05CheckResetAtStrikeInLastFrame()
+    {
+        int[] rolls = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+        foreach (int roll in rolls){
+            actionMaster.Bowl(roll);
+        }
+        actionMaster.Bowl(1);
+        Assert.AreEqual(reset, actionMaster.Bowl(9));
+    }
+
+    [Test]
+    public void T06YouTubeRollsEndInEndGame() {
+        int[] rolls = { 8, 2, 7, 3, 3, 4, 10, 2, 8, 10, 10, 8, 0, 10, 8, 2};
+        foreach (int roll in rolls){
+            actionMaster.Bowl(roll);
+        }
+        Assert.AreEqual(endGame, actionMaster.Bowl(9));
+    }
+    [Test]
+    public void T07GameEndsAtBow20()
+    {
+        int[] rolls = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1 };
+        foreach (int roll in rolls)
+        {
+            actionMaster.Bowl(roll);
+        }
+        Assert.AreEqual(endGame, actionMaster.Bowl(1));
     }
 }
--- Assets/Scripts/ActionMaster.cs
+++ Assets/Scripts/ActionMaster.cs
@@ -5,12 +5,26 @@ public class ActionMaster : MonoBehaviour {
 
     public enum Action {Tidy,Reset,EndTurn,EndGame };
 
-    //private int[] bowls = new int[21];
+    private int[] bowls = new int[21];
     private int bowl = 1;
 
     public Action Bowl(int pins){
 
-        if(pins<0 || pins > 10){ throw new UnityException("Invalid pins.");}  
+        if(pins<0 || pins > 10){ throw new UnityException("Invalid pins.");}
+
+        bowls[bowl - 1] = pins;
+
+        if (bowl == 21){
+            return Action.EndGame;
+        }
+
+        //Hanld last-frame special cases.
+        if(bowl >= 19 && Bowl21Awarded()) {
+            bowl++;
+            return Action.Reset;
+        }else if(bowl == 20 && !Bowl21Awarded()){
+            return Action.EndGame;
+        }
 
         if (pins == 10){
             bowl += 2;
@@ -27,4 +41,9 @@ public class ActionMaster : MonoBehaviour {
 
         throw new UnityException("Not sure what action to return! ");
     }
+
+    private bool Bowl21Awarded(){
+        return (bowls[19 - 1] + bowls[20 - 1] >= 10);
+    }
+
 }
