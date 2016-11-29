3c3f181592d1c4754f24c43f3e81114828e28368
Mehmet Cengiz <m.cengiz4716@gmail.com>
2016-11-29 14:10:37 +0300

    Creating more testable architecture
    ScoreMaster created.

---
+++ Assets/Scripts/ScoreMaster.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class ScoreMaster {
+
+    public static List<int> ScoreFrames(List<int> rolls) {
+        List<int> frameList = new List<int>();
+
+
+        return frameList;
+    }
+}
